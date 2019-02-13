018c519e859f931b9a4d7dc27f8d8691afe62750
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-02-13 23:04:31 +0530

    instagram bot

--- instagrambot/instragramlikebot.py
+++ instagrambot/instragramlikebot.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
 import time
 
-driver = webdriver.Firefox()
-# driver = webdriver.Chrome()
 
-driver.set_page_load_timeout(10)
-driver.get("https://www.google.com/")
+class InstagramBot:
 
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Firefox()
 
-driver.find_element_by_name("q").send_keys("Jaspreet Singh Baidwan    ")
-driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
+    def closeBrowser(self):
+        self.driver.close()
+
