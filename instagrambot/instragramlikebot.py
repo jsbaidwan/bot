edb9b0e6df8a76b335e59d4063454a8c936d83ee
jsbaidwan <cgc.jsbaidwan@gmail.com>
2019-02-14 15:22:42 +0530

    go to the instagram page and click on log in

--- instagrambot/instragramlikebot.py
+++ instagrambot/instragramlikebot.py
@@ -13,3 +13,17 @@ class InstagramBot:
     def closeBrowser(self):
         self.driver.close()
 
+    def login(self):
+        driver = self.driver
+        driver.get("https://www.instagram.com/")
+        time.sleep(2)
+        loginButton = driver.find_element_by_link_text('Log in')
+        loginButton.click()
+        loginButton.send_keys(Keys.ENTER)
+        time.sleep(2)
+
+
+
+myInstagramLogin = InstagramBot('fdkjgkdf', 'dfkjg')     # enter your username and password
+myInstagramLogin.login()
+
